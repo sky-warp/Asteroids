3340161564b7f4c41748137f321e4f76cba93157
Andrei Savchenko <guiltyvlr4u@gmail.com>
2025-03-28 19:06:22 +1000

    Game over service clean

--- Assets/_Project/Scripts/GameOverServices/DefaultGameOverService.cs
+++ Assets/_Project/Scripts/GameOverServices/DefaultGameOverService.cs
@@ -1,26 +1,9 @@
-using System;
 using R3;
 
 namespace _Project.Scripts.GameOverServices
 {
-    public class DefaultGameOverService : IDisposable
+    public class DefaultGameOverService
     {
-        public readonly ReactiveProperty<bool> IsGameOver = new(false);
         public readonly Subject<Unit> OnGameOver = new();
-
-        private readonly CompositeDisposable _disposable = new();
-        
-        public DefaultGameOverService()
-        {
-            OnGameOver
-                .Select(_ => true)
-                .Subscribe(isOver => IsGameOver.Value = isOver)
-                .AddTo(_disposable);
-        }
-
-        public void Dispose()
-        {
-            _disposable.Dispose();
-        }
     }
 }
