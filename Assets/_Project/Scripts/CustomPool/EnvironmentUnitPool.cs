3b3a775d76ecce9b1fb4ad554a8b3ec6de028516
Andrei Savchenko <guiltyvlr4u@gmail.com>
2025-04-16 17:27:26 +1000

    Environment units base factory and pool

--- Assets/_Project/Scripts/CustomPool/EnvironmentUnitPool.cs
+++ Assets/_Project/Scripts/CustomPool/EnvironmentUnitPool.cs
@@ -1,63 +1,19 @@
 using System.Collections.Generic;
-using System.Linq;
 using _Project.Scripts.Environment;
 using _Project.Scripts.Factories;
 using UnityEngine;
 
 namespace _Project.Scripts.CustomPool
 {
-    public class EnvironmentUnitPool<T> where T : EnvironmentObject
+    public class EnvironmentUnitPool<T> : BasePool<T> where T : EnvironmentObject
     {
         private Transform _parent;
         private EnvironmentUnitFactory<T> _factory;
         private List<T> Pool { get; }
 
-        public EnvironmentUnitPool(int prewarmObjects,Transform parent , EnvironmentUnitFactory<T> factory)
+        public EnvironmentUnitPool(int prewarmObjects, Transform parent, EnvironmentUnitFactory<T> factory) : base(
+            prewarmObjects, parent, factory)
         {
-            _factory = factory;
-            _parent = parent;
-            Pool = new List<T>();
-
-            for (int i = 0; i < prewarmObjects; i++)
-            {
-                var obj = _factory.Create(_parent);
-                obj.gameObject.SetActive(false);
-                Pool.Add(obj);
-            }
-        }
-        
-        public T Get()
-        {
-            var obj = Pool.FirstOrDefault(obj => !obj.gameObject.activeSelf);
-            
-            if (obj == null)
-                obj = Create();
-
-            obj.gameObject.SetActive(true);
-
-            return obj;
-        }
-
-        public void Release(T obj)
-        {
-            obj.transform.localPosition = Vector3.zero;
-            obj.transform.localRotation = Quaternion.identity;
-            obj.gameObject.SetActive(false);
-        }
-
-        public void ReleaseAll()
-        {
-            foreach (var t in Pool)
-            {
-                Release(t);
-            }
-        }
-        
-        private T Create()
-        {
-            var obj = _factory.Create(_parent);
-            Pool.Add(obj);
-            return obj;
         }
     }
 }
--- Assets/_Project/Scripts/Factories/EnvironmentUnitFactory.cs
+++ Assets/_Project/Scripts/Factories/EnvironmentUnitFactory.cs
@@ -3,20 +3,20 @@ using UnityEngine;
 
 namespace _Project.Scripts.Factories
 {
-    public class EnvironmentUnitFactory<T> where T : EnvironmentObject
+    public class EnvironmentUnitFactory<T> : BaseMonoFactory<T> where T : EnvironmentObject
     {
         private T _unitPrefab;
         private float _unitSpeed;
         private int _unitScore;
 
-        public EnvironmentUnitFactory(T monoPrefab, float unitSpeed, int unitScore)
+        public EnvironmentUnitFactory(T monoPrefab, float unitSpeed, int unitScore) : base(monoPrefab)
         {
             _unitPrefab = monoPrefab;
             _unitSpeed = unitSpeed;
             _unitScore = unitScore;
         }
         
-        public T Create(Transform parent)
+        public override T Create(Transform parent)
         {
             var instance = GameObject.Instantiate(_unitPrefab, parent);
             instance.SetSpeed(_unitSpeed);
